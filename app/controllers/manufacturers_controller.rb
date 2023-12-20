c89bb73ce41785f3a5738506a79f180dac243339
kevinjolley91 <jolley427@gmail.com>
2023-12-20 17:30:38 -0500

    adds routes and CRUD actions to manufacturers

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,2 +1,38 @@
 class ManufacturersController < ApplicationController
+  def index
+    @manufacturers = Manufacturer.all
+    render :index
+  end
+
+  def show
+    @manufacturer = Manufacturer.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @manufacturer = Manufacturer.create(
+      name: params[:name],
+      year_created: params[:year_created],
+      country: params[:country],
+      img_logo: params[:img_logo],
+    )
+    render :show
+  end
+
+  def update
+    @manufacturer = Manufacturer.find_by(id: params[:id])
+    @manufacturer.update(
+      name: params[:name] || @manufacturer.name,
+      year_created: params[:year_created] || @manufacturer.year_created,
+      country: params[:country] || @manufacturer.country,
+      img_logo: params[:img_logo] || @manufacturer.img_logo,
+    )
+    render :show
+  end
+
+  def delete
+    @manufacturer = Manufacturer.find_by(id params[:id])
+    @manufacturer.destroy
+    render json: { message: "Manufacturer removed." }
+  end
 end
---
+++ app/views/manufacturers/_manufacturer.json.jbuilder
@@ -0,0 +1,5 @@
+json.id manufacturer.id
+json.name manufacturer.name
+json.year_created manufacturer.year_created
+json.country manufacturer.country
+json.img_logo manufacturer.img_logo
